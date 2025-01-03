e9310c5cd627f328501fd813904c69d4a1fbfde3
tuilakhanh <bacdau852@gmail.com>
2025-01-03 01:02:54 +0000

    Move to shadcn

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,34 +1,87 @@
 /** @type {import('tailwindcss').Config} */
+
+
 export default {
-  darkMode: "class",
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    screens: {
-      'xs' : '350px',
-      // =>
-      'sm': '640px',
-      // => @media (min-width: 640px) { ... }
+	darkMode: "selector",
+	content: [
+	"./index.html",
+	"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		screens: {
+			'xs' : '350px',
+			// =>
+			'sm': '640px',
+			// => @media (min-width: 640px) { ... }
 
-      'md': '768px',
-      // => @media (min-width: 768px) { ... }
+			'md': '768px',
+			// => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
-      // => @media (min-width: 1024px) { ... }
+			'lg': '1024px',
+			// => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
-      // => @media (min-width: 1280px) { ... }
+			'xl': '1280px',
+			// => @media (min-width: 1280px) { ... }
 
-      '2xl': '1536px',
-      // => @media (min-width: 1536px) { ... }
-    },
-    extend: {
-      backgroundImage: theme => ({
-        'banner-texture': "url('https://res.cloudinary.com/droondbdu/image/upload/v1702314723/360_F_460444211_E7j3njYE705Rk1guKz9LKh58gFgiTybV_lpp3kr.jpg)",
-      })
+			'2xl': '1536px',
+			// => @media (min-width: 1536px) { ... }
+		},
+		extend: {
+			backgroundImage: theme => ({
+			'banner-texture': "url('https://res.cloudinary.com/droondbdu/image/upload/v1702314723/360_F_460444211_E7j3njYE705Rk1guKz9LKh58gFgiTybV_lpp3kr.jpg)",
+			})
+		}
+	},
+    plugins: [require("tailwindcss-animate")],
+    theme: {
+    	extend: {
+    		borderRadius: {
+    			lg: 'var(--radius)',
+    			md: 'calc(var(--radius) - 2px)',
+    			sm: 'calc(var(--radius) - 4px)'
+    		},
+    		colors: {
+    			background: 'hsl(var(--background))',
+    			foreground: 'hsl(var(--foreground))',
+    			card: {
+    				DEFAULT: 'hsl(var(--card))',
+    				foreground: 'hsl(var(--card-foreground))'
+    			},
+    			popover: {
+    				DEFAULT: 'hsl(var(--popover))',
+    				foreground: 'hsl(var(--popover-foreground))'
+    			},
+    			primary: {
+    				DEFAULT: 'hsl(var(--primary))',
+    				foreground: 'hsl(var(--primary-foreground))'
+    			},
+    			secondary: {
+    				DEFAULT: 'hsl(var(--secondary))',
+    				foreground: 'hsl(var(--secondary-foreground))'
+    			},
+    			muted: {
+    				DEFAULT: 'hsl(var(--muted))',
+    				foreground: 'hsl(var(--muted-foreground))'
+    			},
+    			accent: {
+    				DEFAULT: 'hsl(var(--accent))',
+    				foreground: 'hsl(var(--accent-foreground))'
+    			},
+    			destructive: {
+    				DEFAULT: 'hsl(var(--destructive))',
+    				foreground: 'hsl(var(--destructive-foreground))'
+    			},
+    			border: 'hsl(var(--border))',
+    			input: 'hsl(var(--input))',
+    			ring: 'hsl(var(--ring))',
+    			chart: {
+    				'1': 'hsl(var(--chart-1))',
+    				'2': 'hsl(var(--chart-2))',
+    				'3': 'hsl(var(--chart-3))',
+    				'4': 'hsl(var(--chart-4))',
+    				'5': 'hsl(var(--chart-5))'
+    			}
+    		}
+    	}
     }
-  },
-  plugins: [],
 }
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+ 
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.mp4'],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  }
 })
-
